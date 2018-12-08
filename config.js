f1f3ccf79a9f7ff8f4b75a8e1fb7c242a3242023
Liz Krane <learningnerd@gmail.com>
2018-12-07 23:20:39 -0800

    Refactor constants, key-move mappings

---
+++ config.js
@@ -0,0 +1,32 @@
+import {KEYS, MOVES} from "./constants.js"
+
+// Mapping keyboard controls to game moves
+// Reference: https://strategywiki.org/wiki/Tetris/Controls
+export const KEY_MOVE_MAP = {
+  [KEYS.LEFT]: MOVES.LEFT,
+  [KEYS.RIGHT]: MOVES.RIGHT,
+
+  [KEYS.UP]: MOVES.ROTATE_CLOCKWISE,
+  [KEYS.X]: MOVES.ROTATE_CLOCKWISE,
+
+  [KEYS.Z]: MOVES.ROTATE_COUNTER_CLOCKWISE,
+  [KEYS.CTRL]: MOVES.ROTATE_COUNTER_CLOCKWISE,
+
+  [KEYS.DOWN]: MOVES.SOFT_DROP,
+  [KEYS.SPACE]: MOVES.HARD_DROP,
+};
+
+// Prevent default browser behavior for these keys
+// NOTE: do this for all game controls except the CTRL key, because otherwise
+//       I can't do Ctrl+Shift+J to open the JS console while testing the game!
+export const OVERRIDE_KEYS = [
+  KEYS.LEFT,
+  KEYS.RIGHT,
+  KEYS.DOWN,
+  KEYS.UP,
+  KEYS.SPACE,
+  KEYS.X,
+  KEYS.Z
+];
+
+
---
+++ constants.js
@@ -0,0 +1,65 @@
+// Normalized key names used in KEY_MAP defined below
+export const KEYS = {
+  UP: "upArrow",
+  DOWN: "downArrow",
+  LEFT: "leftArrow",
+  RIGHT: "rightArrow",
+  X: "x",
+  Z: "z",
+  CTRL: "control",
+  SPACE: "space"
+};
+
+// KEY_MAP --
+// Normalizing KeyboardEvent values for "key" and "keyCode" properties
+// References:
+//  https://w3c.github.io/uievents/
+//  https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+//  https://medium.com/@uistephen/keyboardevent-key-for-cross-browser-key-press-check-61dbad0a067a
+//  
+//  Summary:
+//    - Modern browsers implement "key" property, but IE/Edge use different values for some keys
+//    - keyCode "is supported in effectively all browsers (since IE6+, Firefox 2+, Chrome 1+ etc)"
+//      via https://caniuse.com/#search=KeyboardEvent.keyCode
+export const KEY_MAP = {
+  "ArrowUp": KEYS.UP,
+  "Up": KEYS.UP,
+  "38": KEYS.UP,
+
+  "ArrowDown": KEYS.DOWN,
+  "Down": KEYS.DOWN,
+  "40": KEYS.DOWN,
+
+  "ArrowLeft": KEYS.LEFT,
+  "Left": KEYS.LEFT,
+  "37": KEYS.LEFT,
+
+  "ArrowRight": KEYS.RIGHT,
+  "Right": KEYS.RIGHT,
+  "39": KEYS.RIGHT,
+
+  " ": KEYS.SPACE,
+  "Spacebar": KEYS.SPACE,
+  "32": KEYS.SPACE,
+  
+  "Control": KEYS.CTRL,
+  "17": KEYS.CTRL, // left control (still need to check about the right one, lol)
+
+  "x": KEYS.X,
+  "88": KEYS.X,
+  "z": KEYS.Z,
+  "90": KEYS.Z
+};
+
+// Constants for naming game moves/actions
+export const MOVES = {
+  LEFT: "left",
+  RIGHT: "right",
+  SOFT_DROP: "soft-drop",
+  LEFT_SOFT_DROP: "left-soft-drop",
+  RIGHT_SOFT_DROP: "right-soft-drop",
+  HARD_DROP: "hard-drop",
+  ROTATE_CLOCKWISE: "rotate-clockwise",
+  ROTATE_COUNTER_CLOCKWISE: "rotate-counterclockwise"
+};
+
